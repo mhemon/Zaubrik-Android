0507f9466feff9265811206d57048d602c99e626
mhemon <mhemon02@gmail.com>
2024-03-20 21:22:55 +0100

    code uploaeded

---
+++ app/src/main/java/de/zaubrik/MainActivity.kt
@@ -0,0 +1,62 @@
+package de.zaubrik
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.webkit.WebViewClientCompat
+import de.zaubrik.ui.theme.ZaubrikTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ZaubrikTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WebViewScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewScreen() {
+
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                settings.loadWithOverviewMode = true
+                settings.useWideViewPort = true
+                settings.setSupportZoom(true)
+            }
+        },
+        update = { webView ->
+            webView.loadUrl("https://web-components.zaubrik.de/")
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ZaubrikTheme {
+        WebViewScreen()
+    }
+}
